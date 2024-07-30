c87b5c7c676b7b42c9188213df2ef8a9b990f89d
robiuzzaman4 <robiuzzaman4@gmail.com>
2024-07-31 00:38:12 +0600

    feat: update navbar

--- src/components/block/navbar.tsx
+++ src/components/block/navbar.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Navlink from "./navlink";
 import { Button } from "../ui/button";
 import MobileNavbar from "./mobile-navbar";
+import Image from "next/image";
 
 export const navlinks = [
   {
@@ -22,9 +23,19 @@ export const navlinks = [
 const Navbar = () => {
   return (
     <nav className="w-full fixed top-0 bg-background border-b border-b-border">
-      <div className="w-full max-w-screen-lg mx-auto px-4 lg:px-0 py-4 flex items-center justify-between">
-        <Link href={"/"} className="text-2xl font-semibold tracking-tighter">
-          Auth
+      <div className="w-full max-w-screen-lg mx-auto px-4 xl:px-0 py-4 flex items-center justify-between">
+        <Link
+          href={"/"}
+          className="text-2xl font-semibold tracking-tighter flex items-center gap-1"
+        >
+          <Image
+            src={"/shield.svg"}
+            alt="sheild"
+            height={48}
+            width={48}
+            className="h-6 w-6 object-cover"
+          />
+          <span>Auth</span>
         </Link>
 
         <div className="hidden lg:flex items-center gap-4">
